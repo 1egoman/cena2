9c57adbf8f40c40345a2e76c31109b3ad1c1f6a4
1egoman <rsg1egoman@gmail.com>
2015-04-09 10:41:33 -0400

    grocery list, recipe list, and other list seperation, and also sorting of grocery lists in the controller

--- public/js/app.js
+++ public/js/app.js
@@ -92,6 +92,38 @@ app.controller("ListController", function($scope, $routeParams, ListService, Foo
     root.doPrintableList();
   });
 
+  // return all lists that have the specified tag included
+  root.getListsByTag = function(lists, tag) {
+    // if tag is set, look for everything with that tag
+    // otherwise, get everthing that isn't a tag
+      out = _.filter(lists, function(l) {
+        if (tag) {
+          return l.tags.indexOf(tag) !== -1;
+        } else {
+          return l.tags.indexOf("grocery") === -1 && l.tags.indexOf("recipe") === -1;
+        }
+      });
+
+
+    // if it's a grocery list, with hopefully a date in the name
+    if (tag === "grocery") {
+      // sort all grocery lists
+      out = _.sortBy(out, function(n) {
+        // find dates by regex
+        dates = n.name.match(/[\d]{1,2}[\.\/-]?[\d]{1,2}[\.\/-]?[\d]{2,4}?/gi);
+        if (dates.length) {
+          // format the regex output into a date,
+          // and get the timestamp to compare
+          preDate = dates[0].split(/[\.\/-]/gi);
+          if (preDate.length < 1) preDate = dates[0].match(/[.]{2}/gi);
+          return new Date(preDate.join("/")).getTime()
+        };
+      }).reverse();
+    };
+
+    return out;
+  };
+
   // list fuzzy searching
   root.matchesSearchString = function(list, filter) {
     // if there's no filter, return true
